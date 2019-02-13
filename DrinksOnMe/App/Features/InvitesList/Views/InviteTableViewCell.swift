8f33de9b1df99591eaabc1fe54fc715c94c43d7b
Bryan A <bolivarbryan@gmail.com>
2019-02-13 03:11:53 -0500

    removed unused functions

--- DrinksOnMe/App/Features/InvitesList/Views/InviteTableViewCell.swift
+++ DrinksOnMe/App/Features/InvitesList/Views/InviteTableViewCell.swift
@@ -7,11 +7,4 @@ class InviteTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
